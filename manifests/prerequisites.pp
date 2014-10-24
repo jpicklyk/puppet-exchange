d2556e580e6121aceb1cd55c486ee3dcdee0ae05
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-24 14:18:31 -0400

    changing resource name

--- manifests/prerequisites.pp
+++ manifests/prerequisites.pp
@@ -23,8 +23,8 @@ class exchange::prerequisites(
     default     => ["NET-Framework","RSAT-ADDS","Web-Server","Web-Basic-Auth","Web-Windows-Auth","Web-Metabase","Web-Net-Ext","Web-Lgcy-Mgmt-Console","WAS-Process-Model","RSAT-Web-Server","Web-ISAPI-Ext","Web-Digest-Auth","Web-Dyn-Compression","NET-HTTP-Activation","RPC-Over-HTTP-Proxy"],
   }
   notify{'$features':}
-  windowsfeature{'$exrole':
-    feature_name  => ["NET-Framework","RSAT-ADDS","Web-Server"],
+  windowsfeature{'exchange':
+    feature_name  => ['NET-Framework','RSAT-ADDS','Web-Server'],
   }
   contain windowsfeature
 }
