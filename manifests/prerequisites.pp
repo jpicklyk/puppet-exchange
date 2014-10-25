933b097856bed5907bda9ef002a8defd908ebc0d
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-25 11:15:45 -0400

    Changing the windowsfeature format

--- manifests/prerequisites.pp
+++ manifests/prerequisites.pp
@@ -5,7 +5,7 @@ class exchange::prerequisites(
   $update         = 'filterpacksp2010-kb2687447-fullfile-x64-en-us.exe',
 ) {
   validate_re($exrole, '^(unified|cas|mailbox)$', 'Unsupported role, choose 1 of \'unified\', \'cas\', or \'mailbox\'')
-
+  
   package { 'Microsoft Filter Pack 2.0':
     ensure => 'installed',
     source => "${directory}${filename}",
@@ -22,9 +22,7 @@ class exchange::prerequisites(
     'mailbox'   => ["NET-Framework","RSAT-ADDS","Web-Server","Web-Basic-Auth","Web-Windows-Auth","Web-Metabase","Web-Net-Ext","Web-Lgcy-Mgmt-Console","WAS-Process-Model","RSAT-Web-Server"],    
     default     => ["NET-Framework","RSAT-ADDS","Web-Server","Web-Basic-Auth","Web-Windows-Auth","Web-Metabase","Web-Net-Ext","Web-Lgcy-Mgmt-Console","WAS-Process-Model","RSAT-Web-Server","Web-ISAPI-Ext","Web-Digest-Auth","Web-Dyn-Compression","NET-HTTP-Activation","RPC-Over-HTTP-Proxy"],
   }
-  notify{'$features':}
-  windowsfeature{'exchange':
-    feature_name  => ['NET-Framework','RSAT-ADDS','Web-Server'],
-  }
+  
+  windowsfeature{$features:}
   contain windowsfeature
 }
