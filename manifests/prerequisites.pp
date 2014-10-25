42d52f85f00a15762e0a25982c8db605c8c51727
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-25 11:19:23 -0400

    Testing array

--- manifests/prerequisites.pp
+++ manifests/prerequisites.pp
@@ -22,7 +22,7 @@ class exchange::prerequisites(
     'mailbox'   => ["NET-Framework","RSAT-ADDS","Web-Server","Web-Basic-Auth","Web-Windows-Auth","Web-Metabase","Web-Net-Ext","Web-Lgcy-Mgmt-Console","WAS-Process-Model","RSAT-Web-Server"],    
     default     => ["NET-Framework","RSAT-ADDS","Web-Server","Web-Basic-Auth","Web-Windows-Auth","Web-Metabase","Web-Net-Ext","Web-Lgcy-Mgmt-Console","WAS-Process-Model","RSAT-Web-Server","Web-ISAPI-Ext","Web-Digest-Auth","Web-Dyn-Compression","NET-HTTP-Activation","RPC-Over-HTTP-Proxy"],
   }
-  
-  windowsfeature{$features:}
+  $test = ['NET-Framework','RSAT-ADDS','Web-Server']
+  windowsfeature{$test:}
   contain windowsfeature
 }
