906570e3cb8f88e8ce08b6c0c4ad9987a1668454
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-25 11:34:18 -0400

    test single feature

--- manifests/prerequisites.pp
+++ manifests/prerequisites.pp
@@ -23,6 +23,6 @@ class exchange::prerequisites(
     default     => ["NET-Framework","RSAT-ADDS","Web-Server","Web-Basic-Auth","Web-Windows-Auth","Web-Metabase","Web-Net-Ext","Web-Lgcy-Mgmt-Console","WAS-Process-Model","RSAT-Web-Server","Web-ISAPI-Ext","Web-Digest-Auth","Web-Dyn-Compression","NET-HTTP-Activation","RPC-Over-HTTP-Proxy"],
   }
   $test = ['NET-Framework','RSAT-ADDS','Web-Server']
-  windowsfeature{$test:}
-  #contain windowsfeature
+  windowsfeature{'NET-Framework':}
+  contain windowsfeature
 }
