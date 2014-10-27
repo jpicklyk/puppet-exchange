fab7cc8f1aade54f221d79a98ce5bcc8f65c5bc7
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-27 11:45:08 -0400

    Install manifest

---
+++ manifests/install.pp
@@ -0,0 +1,34 @@
+class exchange::install (
+  $exrole = undef,
+  $path,
+  $orgname,
+) {
+  validate_bool(prepareshema)
+  validate_string(orgname)
+  
+  $setuprole = $exrole ? {
+    default => 'CA,MB,HT,MT'
+  }
+  
+  
+  exec{'Schema Prep':
+    command   => "setup.com /PS",
+    path      => "${path}",
+    provider  => powershell,
+    unless    => 'Try {Get-ADObject $("CN=ms-Exch-Schema-Version-Pt,"+$((Get-ADRootDSE).NamingContexts | Where-Object {$_ -like "*Schema*"}))}Catch {exit 1}',
+  } ->
+  
+  exec{'Doamin Prep':
+    command   => "setup.com /PrepareAD /OrganizationName:${orgname}",
+    path      => "${path}",
+    provider  => powershell,
+    require => Exec['Schema Prep'],
+  } ->
+  
+  exec{'Install Role':
+    command   => "${path}\\setup.com /mode:install /role:${setuprole} /organizationName:${orgname}",
+    provider  => powershell,
+    require => Exec['Domain Prep'],
+  }
+  
+}
