1d5fdb7668b457b14cf03b4cb6337f7b6c80655f
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-27 12:28:54 -0400

    Adding a test to the installation exec to see if exchange is installed

--- manifests/install.pp
+++ manifests/install.pp
@@ -16,18 +16,19 @@ class exchange::install (
     path      => "${path}",
     provider  => powershell,
     unless    => 'Try {Get-ADObject $("CN=ms-Exch-Schema-Version-Pt,"+$((Get-ADRootDSE).NamingContexts | Where-Object {$_ -like "*Schema*"}))}Catch {exit 1}',    
-  } ->
+  } ~>
   
   exec{'Doamin Prep':
     command   => "setup.com /PrepareAD /OrganizationName:${orgname}",
     path      => "${path}",
     provider  => powershell,
     require => Exec['Schema Prep'],
-  } ~>
+  } ->
   
   exec{'Install Role':
     command   => "${path}\\setup.com /mode:install /role:${setuprole} /organizationName:${orgname}",
     provider  => powershell,
+    unless    => 'Try {Get-ExchangeServer}Catch{exit 1}',
     timeout   => 0
   }
   
